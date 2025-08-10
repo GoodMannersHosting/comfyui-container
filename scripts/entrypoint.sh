9877cd07aa8d2a940914ef23b1fac76cfafdbda1
Dan Manners <daniel.a.manners@gmail.com>
2025-08-10 15:41:53 -0400

    fix: Was accidentally killing the server before actually starting it
    Signed-off-by: Dan Manners <daniel.a.manners@gmail.com>

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -3,9 +3,9 @@
 # Copy the ComfyUI Manager to the custom_nodes directory
 cp -r /app/comfyui-manager \
     /app/ComfyUI/custom_nodes/comfyui-manager || \
-    echo "ERROR: Failed to copy ComfyUI Manager" && exit 1
+    echo "ERROR: Failed to copy ComfyUI Manager"
 
 # Run the ComfyUI entrypoint script
-python3 -u main.py --listen 0.0.0.0 \
+python3 -u main.py \
     --front-end-version Comfy-Org/ComfyUI_frontend@latest \
-    /app/ComfyUI/scripts/entrypoint.py ${CLI_ARGS}
+    --listen 0.0.0.0  ${CLI_ARGS}
